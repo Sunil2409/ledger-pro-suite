81af3709ffa70cf49e0a0865a42776933c7930ad
Sunil2409 <esunilkumar2409@gmail.com>
2026-02-03 21:53:56 +0530

    Fix logout redirect

--- finance_portfolio/urls.py
+++ finance_portfolio/urls.py
@@ -14,6 +14,6 @@ urlpatterns = [
     
     # Authentication URLs
     path('login/', auth_views.LoginView.as_view(template_name='registration/login.html'), name='login'),
-    path('logout/', auth_views.LogoutView.as_view(), name='logout'),
+    path('logout/', auth_views.LogoutView.as_view(next_page='login'), name='logout'),
     path('signup/', include('django.contrib.auth.urls')),
 ]
